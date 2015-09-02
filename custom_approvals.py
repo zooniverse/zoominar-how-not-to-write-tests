2b7104ed7ae92f3123dd2ff24def3cba670dac23
Marten Veldthuis <marten@veldthuis.com>
2015-09-02 11:53:49 +0100

    clean

--- custom_approvals.py
+++ custom_approvals.py
@@ -1,7 +1,7 @@
+import json
+
 def something_complicated():
     return [1,2,37,42]
-
-import json
 
 with open('something_complicated_approved.json', 'wb') as f:
     json.dump(something_complicated(), f)
